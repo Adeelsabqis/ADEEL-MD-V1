a95957f2f3e93dabc459974feb9545ca79a6e8a1
Adeelsabqis <141720474+Adeelsabqis@users.noreply.github.com>
2025-09-05 20:31:55 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'madewith❤️;asb',
+  author: 'reddragon-dfs',
+  botName: "adeel md",
+  botOwner: 'MR-ADEEL', // Your name
+  ownerNumber: '923174838990', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
